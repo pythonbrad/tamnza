765e93d7979a58cb605d741f44e9437e9f5a5e3c
Fomegne <fomegnemeudje@outlook.com>
2022-02-06 19:27:24 +0100

    feat: Update database Update the SQL code and the DAO

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 # TODO LIST
 - Classroom application
-	+ [ ] Build the database
+	- [ ] Build the database
+		- [x] SQL code
+		- [x] Write a base DAO
 	+ [ ] Build the models
 	- [ ] Build the routers
 		- [x] Base
--- tamnza/tamnza/database.php
+++ tamnza/tamnza/database.php
@@ -5,13 +5,89 @@ namespace Tamnza\Database;
 // Database Access Object
 class BaseDAO
 {
-    private $db;
+    private \PDO $db;
 
-    public function __construct()
-    {
+    public function __construct(
+        private string $tb_name,
+        private string $pk,
+        private array $fields,
+    ) {
         $this->db = match (DATABASES["default"]['ENGINE']) {
             "mysql" => new \PDO("mysql:host=" . DATABASES["default"]["HOST"] . ";port=" . DATABASES["default"]["PORT"] . ";dbname=" . DATABASES["default"]["NAME"], DATABASES["default"]["USERNAME"], DATABASES["default"]["PASSWORD"]),
             "sqlite" => new \PDO("sqlite:" . DATABASES["default"]["NAME"]),
         };
+    }
+
+    // This method verify the validity of the fields given
+    private function fieldsVerification(array $fields)
+    {
+        foreach (array_keys($fields) as $field) {
+            if (!in_array($field, $this->fields) && $field != $this->pk) {
+                throw new \Exception("$field not found in the fields of table " . $this->tb_name);
+            }
+        }
+    }
+
+    public function insert(array $fields): int
+    {
+        $this->fieldsVerification($fields);
+
+        // We build the query
+        $sql = 'INSERT INTO ' . $this->tb_name . '(' . $this->pk . ', ' . join(', ', array_keys($fields)) . ') VALUES (null, ' . trim(str_repeat('?,', count($fields)), ',') . ');';
+
+        // We execute the query
+        $this->db->prepare($sql)->execute(array_values($fields));
+
+        // We get his id
+        $sql = 'SELECT LAST_INSERT_ID();';
+
+        return $this->db->query($sql)->fetch()[0];
+    }
+
+    public function update(int $id, array $fields): void
+    {
+        $this->fieldsVerification($fields);
+
+        // We build the query
+        $sql = 'UPDATE ' . $this->tb_name . ' SET ' . $this->pk . "=$id,";
+
+        $sql .= join('=?,', array_keys($fields)) . '=?';
+
+        $sql = trim($sql, ',') . ' WHERE ' . $this->pk . '=' . $id . ';';
+
+        // We execute the query
+        $this->db->prepare($sql)->execute(array_values($fields));
+    }
+
+    public function select(array $fields, int $limit): array
+    {
+        // We remove null data
+        $fields = array_filter($fields, function ($var) {
+            return !is_null($var);
+        });
+
+        $this->fieldsVerification($fields);
+
+        // We build the query
+        $sql = 'SELECT * FROM ' . $this->tb_name . ((count($fields) > 0) ? ' WHERE ' : '');
+
+        foreach ($fields as $field => $value) {
+            $sql .= $field . '=? AND ';
+        }
+
+        $sql = trim($sql, 'AND ') . (($limit > -1) ? (' LIMIT ' . $limit) : '') . ';';
+
+        // We execute the query
+        $stmt =  $this->db->prepare($sql);
+        $stmt->execute(array_values($fields));
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function delete(int $id)
+    {
+        // We build the query
+        $sql = 'DELETE FROM ' . $this->tb_name . ' WHERE ' . $this->pk . '=' . $id . ';';
+        $this->db->query($sql);
     }
 }
