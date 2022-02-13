3a97dfa836f59b1b9f5acf4c70bb20ae6ac787dd
Fomegne <fomegnemeudje@outlook.com>
2022-02-13 16:21:53 +0100

    refactor: Apply good practice https://www.php.net/manual/en/language.types.callable.php#example-75

--- tamnza/classroom/controllers/Classroom.php
+++ tamnza/classroom/controllers/Classroom.php
@@ -6,44 +6,42 @@ require_once(dirname(__FILE__) . '/../models/User.php');
 
 class Classroom
 {
-    public function __construct()
+    public function home()
     {
-        $this->home = function () {
-            require(dirname(__FILE__) . '/../views/home.php');
-        };
+        require(dirname(__FILE__) . '/../views/home.php');
+    }
 
-        $this->login = function () {
+    public function login()
+    {
+        $errors = array();
 
-            $errors = array();
-
-            if ($_POST) {
-                // We verify if the form is valid
-                if (!isset($_POST['username']) || strlen($_POST['username']) == 0) {
-                    $errors['username'] = "This field is required.";
-                }
-                if (!isset($_POST['password']) || strlen($_POST['password']) == 0) {
-                    $errors['password'] = "This field is required.";
-                }
-
-                if (!$errors) {
-                    // We verify if the account credentials is correct
-                    $user = \Tamnza\App\Classroom\Model\User::search(password: $_POST['password'], username: $_POST['username']);
-                    //
-                    if ($user) {
-                        // We save the auth
-                        $_SESSION['is_authenticated'] = true;
-                        $_SESSION['username'] = $user[0]->username;
-                        $_SESSION['is_teacher'] = $user[0]->is_teacher;
-
-                        // We redirect to the home page
-                        header("Location: /", true, 301);
-                    } else {
-                        $errors['login'] = "Please enter a correct username and password. Note that both fields may be case-sensitive.";
-                    }
-                }
+        if ($_POST) {
+            // We verify if the form is valid
+            if (!isset($_POST['username']) || strlen($_POST['username']) == 0) {
+                $errors['username'] = "This field is required.";
+            }
+            if (!isset($_POST['password']) || strlen($_POST['password']) == 0) {
+                $errors['password'] = "This field is required.";
             }
 
-            require(BASE_DIR . 'views/registration/login.php');
-        };
+            if (!$errors) {
+                // We verify if the account credentials is correct
+                $user = \Tamnza\App\Classroom\Model\User::search(password: $_POST['password'], username: $_POST['username']);
+                //
+                if ($user) {
+                    // We save the auth
+                    $_SESSION['is_authenticated'] = true;
+                    $_SESSION['username'] = $user[0]->username;
+                    $_SESSION['is_teacher'] = $user[0]->is_teacher;
+
+                    // We redirect to the home page
+                    header("Location: /", true, 301);
+                } else {
+                    $errors['login'] = "Please enter a correct username and password. Note that both fields may be case-sensitive.";
+                }
+            }
+        }
+
+        require(BASE_DIR . 'views/registration/login.php');
     }
 }
--- tamnza/classroom/urls.php
+++ tamnza/classroom/urls.php
@@ -7,6 +7,6 @@ require(dirname(__FILE__) . '/controllers/Classroom.php');
 $classroom = new \Tamnza\App\Classroom\Controller\Classroom();
 
 $routes = array(
-    new Route("$path", $classroom->home),
-    new Route("$path/login", $classroom->login),
+    new Route("$path", array($classroom, 'home')),
+    new Route("$path/login", array($classroom, 'login')),
 );
