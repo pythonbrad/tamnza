b4f158e2a8a26d317b92dbd5c910c2ca7a875645
Fomegne <fomegnemeudje@outlook.com>
2022-02-20 13:12:22 +0100

    feat: Add teacher page

---
+++ tamnza/classroom/controllers/Teacher.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tamnza\App\Classroom\Controller;
+
+require_once(dirname(__FILE__) . '/../models/User.php');
+
+class Teacher
+{
+    public function signup()
+    {
+        $errors = array();
+
+        if ($_POST) {
+            // We verify if the form is valid
+            if (!isset($_POST['username']) || strlen($_POST['username']) == 0) {
+                $errors['username'] = "This field is required.";
+            }
+            if (!isset($_POST['password1']) || strlen($_POST['password1']) == 0) {
+                $errors['password1'] = "This field is required.";
+            }
+            if (!isset($_POST['password2']) || strlen($_POST['password2']) == 0) {
+                $errors['password2'] = "This field is required.";
+            } else if ($_POST['password1'] != $_POST['password2']) {
+                $errors['password2'] = "The two password fields didn’t match.";
+            }
+
+            if (!$errors) {
+                // We verify if the account username is free
+                $user = \Tamnza\App\Classroom\Model\User::search(username: $_POST['username']);
+                //
+                if ($user) {
+                    $errors['username'] = 'A user with that username already exists.';
+                } else {
+                    $user = new \Tamnza\App\Classroom\Model\User(
+                        username: $_POST['username'],
+                        password: $_POST['password1']
+                    );
+                    if ($user->save()) {
+                        // We redirect to the home page
+                        $_SESSION['messages']['success'] = 'Your account has been created with success. You can login now';
+                        header("Location: /", true, 301);
+                        return;
+                    }
+                }
+            }
+        }
+
+        $user_type = 'Teacher';
+
+        require(BASE_DIR . 'views/registration/signup_form.php');
+    }
+}
--- tamnza/classroom/urls.php
+++ tamnza/classroom/urls.php
@@ -4,13 +4,16 @@ use Tamnza\Core\{Route};
 
 require(dirname(__FILE__) . '/controllers/Classroom.php');
 require(dirname(__FILE__) . '/controllers/Student.php');
+require(dirname(__FILE__) . '/controllers/Teacher.php');
 
 $classroom = new \Tamnza\App\Classroom\Controller\Classroom();
 $student = new \Tamnza\App\Classroom\Controller\Student();
+$teacher = new \Tamnza\App\Classroom\Controller\Teacher();
 
 $routes = array(
     new Route("$path", array($classroom, 'home'), name: 'home'),
     new Route("$path/login", array($classroom, 'login'), name: 'login'),
     new Route("$path/signup", array($classroom, 'signup'), name: 'signup'),
     new Route("$path/student_signup", array($student, 'signup'), name: 'student_signup'),
+    new Route("$path/teacher_signup", array($teacher, 'signup'), name: 'teacher_signup'),
 );
