6ca285e3c0a4372afb7a37a7938872f4717b4f9a
pythonbrad <fomegnemeudje@outlook.com>
2022-03-05 12:16:05 +0100

    Create laravel.yml

---
+++ .github/workflows/install_deps.sh
@@ -0,0 +1,5 @@
+apt-get update -yq
+apt-get install -yq mariadb-server wget libonig-dev
+# Install PHP extensions
+docker-php-ext-install mbstring pdo_mysql
+wget -q -O phpunit https://phar.phpunit.de/phpunit-9.phar
---
+++ .github/workflows/start_database.sh
@@ -0,0 +1,13 @@
+mysqld_safe --skip-grant-tables &
+# We wait the complete
+while ! (mysqlshow && [ $counter -gt 0 ]);
+do
+    counter=$(expr $counter - 1);
+    sleep .1;
+    echo Retry \($counter\)...;
+done
+# We verify if an error is got
+if [ $counter -eq 0 ];
+then
+    exit 1;
+fi
