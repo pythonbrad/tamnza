b96eed1e91767fe8da0cb67472529f3b6fcda214
pythonbrad <fomegnemeudje@outlook.com>
2022-03-07 21:51:21 +0100

    chore: update header (#18)
    New: The header correspond to the output

--- tamnza/index.php
+++ tamnza/index.php
@@ -11,9 +11,11 @@ $router = new Core\Router($_GET['url'] ?? '/');
 $routes = array(
     Core\includes("/", 'tamnza/urls.php'),
     new Core\Route('/error', function () {
+        header("HTTP/1.1 500 Internal Server Error");
         require(BASE_DIR . 'views/500.php');
     }, name: "error"),
     new Core\Route('<path:path>', function () {
+        header("HTTP/1.1 404 Not Found");
         require(BASE_DIR . 'views/404.php');
     }),
 );
