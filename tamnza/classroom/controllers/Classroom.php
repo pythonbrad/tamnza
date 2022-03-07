9d99320316baf689c4a68f8d486c127e7148d8f7
pythonbrad <fomegnemeudje@outlook.com>
2022-03-07 14:04:31 +0100

    feat: Protecting the Views (#14)
    The authentication views redirect the authenticated user to the home views.
    The access to the student and teacher views require the authentication.
    A teacher can't have access to the student view and vice-versa.

--- tamnza/classroom/controllers/Classroom.php
+++ tamnza/classroom/controllers/Classroom.php
@@ -23,32 +23,37 @@ class Classroom
 
     public function login()
     {
-        $errors = array();
+        if (isset($_SESSION['is_authenticated']) && $_SESSION['is_authenticated']) {
+            // We redirect to the home page
+            return header("Location: " . $GLOBALS['router']->url("home"), true, 301);
+        } else {
+            $errors = array();
 
-        if ($_POST) {
-            // We verify if the form is valid
-            if (!isset($_POST['username']) || strlen($_POST['username']) == 0) {
-                $errors['username'] = "This field is required.";
-            }
-            if (!isset($_POST['password']) || strlen($_POST['password']) == 0) {
-                $errors['password'] = "This field is required.";
-            }
+            if ($_POST) {
+                // We verify if the form is valid
+                if (!isset($_POST['username']) || strlen($_POST['username']) == 0) {
+                    $errors['username'] = "This field is required.";
+                }
+                if (!isset($_POST['password']) || strlen($_POST['password']) == 0) {
+                    $errors['password'] = "This field is required.";
+                }
 
-            if (!$errors) {
-                // We verify if the account credentials is correct
-                $user = Model\User::search(password: $_POST['password'], username: $_POST['username']);
-                //
-                if ($user) {
-                    // We save the auth
-                    $_SESSION['is_authenticated'] = true;
-                    $_SESSION['username'] = $user[0]->username;
-                    $_SESSION['user'] = $user[0]->getID();
-                    $_SESSION['is_teacher'] = $user[0]->is_teacher;
+                if (!$errors) {
+                    // We verify if the account credentials is correct
+                    $user = Model\User::search(password: $_POST['password'], username: $_POST['username']);
+                    //
+                    if ($user) {
+                        // We save the auth
+                        $_SESSION['is_authenticated'] = true;
+                        $_SESSION['username'] = $user[0]->username;
+                        $_SESSION['user'] = $user[0]->getID();
+                        $_SESSION['is_teacher'] = $user[0]->is_teacher;
 
-                    // We redirect to the home page
-                    return header("Location: " . $GLOBALS['router']->url("home"), true, 301);
-                } else {
-                    $errors['login'] = "Please enter a correct username and password. Note that both fields may be case-sensitive.";
+                        // We redirect to the home page
+                        return header("Location: " . $GLOBALS['router']->url("home"), true, 301);
+                    } else {
+                        $errors['login'] = "Please enter a correct username and password. Note that both fields may be case-sensitive.";
+                    }
                 }
             }
         }
@@ -58,7 +63,12 @@ class Classroom
 
     public function signup()
     {
-        require(BASE_DIR . 'views/registration/signup.php');
+        if (isset($_SESSION['is_authenticated']) && $_SESSION['is_authenticated']) {
+            // We redirect to the home page
+            return header("Location: " . $GLOBALS['router']->url("home"), true, 301);
+        } else {
+            require(BASE_DIR . 'views/registration/signup.php');
+        }
     }
 
     public function logout()
