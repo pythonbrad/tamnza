f7f732f36ec2f37afd3703ec7190b4fe98edaf1a
Fomegne <fomegnemeudje@outlook.com>
2022-03-09 17:58:59 +0100

    style: update global var convention increase readability increase debugging

--- test2.sh
+++ test2.sh
@@ -1,13 +1,23 @@
-#!/usr/bin/env bash
-# Script to test the Tamnza Application
+#!/bin/bash
+#
+# Perform some verification on the Tamnza project.
+# NB: We don't verify the views, just the working of the controllers.
 
-_cookie_jar=/tmp/cookie
-_header=/tmp/header
+HOST=localhost:8080
+COOKIE_JAR=/tmp/cookie
+HEADER=/tmp/header
+OUTPUT=/tmp/output
 
-start_server()
-{
+#######################################
+# Start the application with the PHP built-in web server.
+# Globals:
+#   HOST
+# Arguments:
+#   None
+#######################################
+start_server() {
     # Start the PHP Server
-    php -S localhost:80 -t tamnza &
+    php -S $HOST -t tamnza &
 
     # We wait the PHP server to start
     counter=3
@@ -24,17 +34,29 @@ start_server()
     fi
 }
 
-stop_server()
-{
+#######################################
+# Stop the PHP built-in web server if running.
+# Globals:
+#   None
+# Arguments:
+#   None
+#######################################
+stop_server() {
     pid=$(pidof php)
     if [ ! -z "$pid" ]; then
         kill $pid
     fi
 }
 
-test_connect()
-{
-    header=$(curl -I localhost --cookie-jar $_cookie_jar -s --show-error)
+#######################################
+# Verify if the home page works.
+# Globals:
+#   None
+# Arguments:
+#   None
+#######################################
+test_connect() {
+    header=$(curl -I $HOST --cookie-jar $COOKIE_JAR -s --show-error)
     echo '--- HEADER ---'
     echo "$header"
     echo '--------------'
@@ -43,10 +65,17 @@ test_connect()
     fi
 }
 
-test_session()
-{
+#######################################
+# Verify if the session has been created by the server.
+# Globals:
+#   HOST
+#   COOKIE_JAR
+# Arguments:
+#   None
+#######################################
+test_session() {
     # We verify if the server set a cookie
-    header=$(curl -I localhost --cookie $_cookie_jar -s --show-error)
+    header=$(curl -I $HOST --cookie $COOKIE_JAR -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
@@ -55,9 +84,15 @@ test_session()
     fi
 }
 
-test_404()
-{
-    header=$(curl -I localhost/?url=/a/b/c/d -s --show-error)
+#######################################
+# Verify if the 404 page works.
+# Globals:
+#   HOST
+# Arguments:
+#   None
+#######################################
+test_404() {
+    header=$(curl -I $HOST/?url=/a/b/c/d -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
@@ -66,9 +101,15 @@ test_404()
     fi
 }
 
-test_500()
-{
-    header=$(curl -I localhost/?url=/error -s --show-error)
+#######################################
+# Verify if the 500 page works.
+# Globals:
+#   HOST
+# Arguments:
+#   None
+#######################################
+test_500() {
+    header=$(curl -I $HOST/?url=/error -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
@@ -77,9 +118,15 @@ test_500()
     fi
 }
 
-test_signup()
-{
-    header=$(curl -I localhost/?url=/signup -s --show-error)
+#######################################
+# Verify if the signup page works
+# Globals:
+#   HOST
+# Arguments:
+#   None
+#######################################
+test_signup() {
+    header=$(curl -I $HOST/?url=/signup -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
@@ -88,9 +135,16 @@ test_signup()
     fi
 }
 
-test_signup_teacher()
-{
-    header=$(curl -I localhost/?url=/teacher_signup -s --show-error)
+#######################################
+# Verify if it's possible to signup as teacher
+# Globals:
+#   HOST
+#   HEADER
+# Arguments:
+#   None
+#######################################
+test_signup_teacher() {
+    header=$(curl -I $HOST/?url=/teacher_signup -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
@@ -99,30 +153,44 @@ test_signup_teacher()
     fi
 
     # Both password don't match
-    if ! (curl localhost/?url=/teacher_signup -d username=teacher\
-     -d password1=teacher -d password2=wrong -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 200' < $_header); then
+    if ! (curl $HOST/?url=/teacher_signup -d username=teacher\
+     -d password1=teacher -d password2=wrong -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 200' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 2
     fi
 
     # Success
-    if ! (curl localhost/?url=/teacher_signup -d username=teacher\
-     -d password1=teacher -d password2=teacher -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 301' < $_header); then
+    if ! (curl $HOST/?url=/teacher_signup -d username=teacher\
+     -d password1=teacher -d password2=teacher -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 301' < $HEADER\
+      && ! grep '^Location: ?url=/error' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 3
     fi
 
     # Username already used
-    if ! (curl localhost/?url=/teacher_signup -d username=teacher\
-     -d password1=teacher -d password2=teacher -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 200' < $_header); then
+    if ! (curl $HOST/?url=/teacher_signup -d username=teacher\
+     -d password1=teacher -d password2=teacher -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 200' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 4
     fi
 }
 
-test_signup_student()
-{
-    header=$(curl -I localhost/?url=/student_signup -s --show-error)
+#######################################
+# Verify if it's possible to signup as student.
+# Globals:
+#   HOST
+#   HEADER
+# Arguments:
+#   None
+#######################################
+test_signup_student() {
+    header=$(curl -I $HOST/?url=/student_signup -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
@@ -131,43 +199,57 @@ test_signup_student()
     fi
 
     # We load subjects
-    subjects=""
+    subjects=''
 
-    for subject in $(
-        curl --url localhost/?url=/student_signup -s --show-error\
-         | grep "form-check-input" |\
-          sed "s/.* value=\"//" | sed 's/\" .*//'); do
+    for subject in $(curl --url $HOST/?url=/student_signup -s --show-error\
+     | grep 'form-check-input'\
+     | sed 's/.* value="//'\
+     | sed 's/".*//'); do
         subjects="$subjects&interests[]=$subject"
     done
     
     # Both password don't match
-    if ! (curl localhost/?url=/student_signup -d username=student\
+    if ! (curl $HOST/?url=/student_signup -d username=student\
      -d password1=student -d password2=wrong --data-raw "${subjects}"\
-      -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 200' < $_header); then
+     -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 200' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 2
     fi
     
     # Success
-    if ! (curl localhost/?url=/student_signup -d username=student\
+    if ! (curl $HOST/?url=/student_signup -d username=student\
      -d password1=student -d password2=student --data-raw "${subjects}"\
-      -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 301' < $_header); then
+     -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 301' < $HEADER\
+      && ! grep '^Location: ?url=/error' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 3
     fi
 
     # Username already used
-    if ! (curl localhost/?url=/student_signup -d username=student\
+    if ! (curl $HOST/?url=/student_signup -d username=student\
      -d password1=student -d password2=student --data-raw "${subjects}"\
-      -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 200' < $_header); then
+     -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 200' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 4
     fi
 }
 
-test_login()
-{
-    header=$(curl -I localhost/?url=/login -s --show-error)
+#######################################
+# Verify if the login page works.
+# Globals:
+#   HOST
+#   HEADER
+# Arguments:
+#   None
+#######################################
+test_login() {
+    header=$(curl -I $HOST/?url=/login -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
@@ -176,46 +258,70 @@ test_login()
     fi
 
     # Wrong data
-    if ! (curl localhost/?url=/login -d username=student\
-     -d password=wrong -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 200' < $_header); then
+    if ! (curl $HOST/?url=/login -d username=student\
+     -d password=wrong -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 200' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 2
     fi
 }
 
-test_login_teacher()
-{
-    if ! (curl localhost/?url=/login --cookie-jar $_cookie_jar -d username=teacher\
-     -d password=teacher -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 301' < $_header); then
+#######################################
+# Verify if it's possible to login as teacher.
+# Globals:
+#   HOST
+#   COOKIE_JAR
+#   HEADER
+# Arguments:
+#   None
+#######################################
+test_login_teacher() {
+    if ! (curl $HOST/?url=/login --cookie-jar $COOKIE_JAR -d username=teacher\
+     -d password=teacher -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 301' < $HEADER\
+      && ! grep '^Location: ?url=/error' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 1
     fi
 
-    # We verify the login
-    header=$(curl -I localhost --cookie $_cookie_jar -s --show-error)
+    # We verify the home redirection
+    header=$(curl -I $HOST --cookie $COOKIE_JAR -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
-    if ! (grep '^Location: ?url=/teacher' <<< $header); then
-        return 1
+    if ! (grep '^HTTP/1.1 301' < $HEADER && ! grep '^Location: ?url=/error' <<< $header); then
+        return 2
     fi
 }
 
-test_login_student()
-{
-    if ! (curl localhost/?url=/login --cookie-jar $_cookie_jar -d username=student\
-     -d password=student -s --show-error -D $_header -o /dev/null\
-      && grep '^HTTP/1.1 301' < $_header); then
+#######################################
+# Verify if it's possible to login as student.
+# Globals:
+#   HOST
+#   COOKIE_JAR
+#   HEADER
+# Arguments:
+#   None
+#######################################
+test_login_student() {
+    if ! (curl $HOST/?url=/login --cookie-jar $COOKIE_JAR -d username=student\
+     -d password=student -s --show-error -D $HEADER &> $OUTPUT\
+      && grep '^HTTP/1.1 301' < $HEADER\
+      && ! grep '^Location: ?url=/error' < $HEADER); then
+        cat $HEADER
+        cat $OUTPUT
         return 1
     fi
 
     # We verify the login
-    header=$(curl -I localhost --cookie $_cookie_jar -s --show-error)
+    header=$(curl -I $HOST --cookie $COOKIE_JAR -s --show-error)
     echo '--- HEADER ---'
     echo "$header";
     echo '--------------'
-    if ! (grep '^Location: ?url=/student' <<< $header); then
-        return 1
+    if ! (grep '^HTTP/1.1 301' < $HEADER && ! grep '^Location: ?url=/error' <<< $header); then
+        return 2
     fi
 }
 
