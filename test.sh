d36cc3378fa4bb72831c22e19d29d0bf7eff7535
Fomegne <fomegnemeudje@outlook.com>
2022-03-09 10:21:13 +0100

    style: update

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 if [ -z $PHPUNIT_CMD ]; then
-    echo "You need to config the PHPUNIT_CMD variable"
+    echo 'You need to config the PHPUNIT_CMD variable'
     exit 1
 fi
 
 # We execute all the tests
 for test in $(find ./tamnza | grep Test.php$); do
-    echo "Testing of " $test...
+    echo 'Testing of ' $test...
     $PHPUNIT_CMD $test --colors auto
     if [ ! $? -eq 0 ]; then
         echo ERROR: $test
